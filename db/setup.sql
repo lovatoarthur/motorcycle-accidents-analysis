9b14345793128bd9efb106caf5390387ad43dc04
arthur lovato <alovato@hydropoint.com>
2025-08-04 18:17:30 -0400

    populate states

--- db/setup.sql
+++ db/setup.sql
@@ -91,3 +91,70 @@ CREATE TABLE person_type (
     type_id INT PRIMARY KEY,
     type_name VARCHAR(255)
 );
+
+-- Create the indexes
+CREATE INDEX idx_accidents_state_id ON accidents (state_id);
+CREATE INDEX idx_accidents_county_id ON accidents (county_id);
+CREATE INDEX idx_accidents_city_id ON accidents (city_id);
+CREATE INDEX idx_vehicles_accident_id ON vehicles (accident_id);
+CREATE INDEX idx_persons_vehicle_id ON persons (vehicle_id);
+CREATE INDEX idx_persons_accident_id ON persons (accident_id);
+
+-- Insert data in States, Vehicle_Body_Type, Person_Type
+-- Accidents, vehicles, persons will be populated in the ETL process
+-- Counties, cities will be populated from GSA Geographical Codes
+
+-- Insert data in States
+INSERT INTO states (state_id, state_name) VALUES
+(1, 'Alabama'),
+(2, 'Alaska'),
+(4, 'Arizona'),
+(5, 'Arkansas'),
+(6, 'California'),
+(8, 'Colorado'),
+(9, 'Connecticut'),
+(10, 'Delaware'),
+(11, 'District of Columbia'),
+(12, 'Florida'),
+(13, 'Georgia'),
+(15, 'Hawaii'),
+(16, 'Idaho'),
+(17, 'Illinois'),
+(18, 'Indiana'),
+(19, 'Iowa'),
+(20, 'Kansas'),
+(21, 'Kentucky'),
+(22, 'Louisiana'),
+(23, 'Maine'),
+(24, 'Maryland'),
+(25, 'Massachusetts'),
+(26, 'Michigan'),
+(27, 'Minnesota'),
+(28, 'Mississippi'),
+(29, 'Missouri'),
+(30, 'Montana'),
+(31, 'Nebraska'),
+(32, 'Nevada'),
+(33, 'New Hampshire'),
+(34, 'New Jersey'),
+(35, 'New Mexico'),
+(36, 'New York'),
+(37, 'North Carolina'),
+(38, 'North Dakota'),
+(39, 'Ohio'),
+(40, 'Oklahoma'),
+(41, 'Oregon'),
+(42, 'Pennsylvania'),
+(43, 'Puerto Rico'),
+(44, 'Rhode Island'),
+(45, 'South Carolina'),
+(46, 'South Dakota'),
+(47, 'Tennessee'),
+(48, 'Texas'),
+(49, 'Utah'),
+(50, 'Vermont'),
+(51, 'Virginia'),
+(53, 'Washington'),
+(54, 'West Virginia'),
+(55, 'Wisconsin'),
+(56, 'Wyoming');
